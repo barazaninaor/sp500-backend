a2891ffcdc405502637beb4cc1f38577db09951f
Naor <barazaninaor@gmail.com>
2026-06-15 12:43:09 +0300

    Fix BSON serialization error

--- routes/stocks.py
+++ routes/stocks.py
@@ -1,14 +1,19 @@
 from flask import Blueprint, jsonify
 from database import companies_collection, sectors_collection
+from bson import json_util
+import json
 
 stocks_bp = Blueprint('stocks', __name__)
+
+# פונקציית עזר להמרה בטוחה ל-JSON
+def safe_jsonify(data):
+    return json.loads(json_util.dumps(data))
 
 # 1. GET all stocks
 @stocks_bp.route('/stocks', methods=['GET'])
 def get_all_stocks():
-    # מיון לפי שווי שוק מהגבוה לנמוך
     stocks = list(companies_collection.find({}).sort("market_data.market_cap", -1))
-    return jsonify(stocks), 200
+    return jsonify(safe_jsonify(stocks)), 200
 
 # 2. GET all stocks with sectors
 @stocks_bp.route('/stocks-with-sectors', methods=['GET'])
@@ -27,10 +32,10 @@ def get_stocks_with_sectors():
         }
     ]
     stocks = list(companies_collection.aggregate(pipeline))
-    return jsonify(stocks), 200
+    return jsonify(safe_jsonify(stocks)), 200
 
 # 3. GET all sectors
 @stocks_bp.route('/sectors', methods=['GET'])
 def get_all_sectors():
     sectors = list(sectors_collection.find({}))
-    return jsonify(sectors), 200
+    return jsonify(safe_jsonify(sectors)), 200
